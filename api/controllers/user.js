3e6507634e6b596f9c1ec81bc76259ee954dbbb6
diegoolliveira7 <diegoolliveira1999@gmail.com>
2024-04-15 09:18:50 -0300

    configuração do back

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -3,5 +3,9 @@ import { db } from "../db.js";
 export const getUsers = (_, res) => {
     const q = "select * from ususarios";
 
-    
-}
+    db.query(q, (erro, data) => {
+        if (erro) return res.json(erro);
+
+        return res.status(200).json(data);
+    });
+};
