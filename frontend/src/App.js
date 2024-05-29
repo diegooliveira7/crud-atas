3411243da05fc0622d966df79e67f4dab2b40452
diegoolliveira7 <diegoolliveira1999@gmail.com>
2024-05-29 17:10:09 -0300

    configuração do front

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+import { toast, ToastContainer} from "react-toastify"
+import GlobalStyle from './styles/global';
+import "react-toastify/dist/ReactToastfy.css";
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <ToastContainer autoClose={3000} position={toast.POSITION.BOTTOM_LEFT} />
+      <GlobalStyle />
+    </>
   );
 }
 
