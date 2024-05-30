49c2171681576beea306abf0d8bd8ce37d6a421d
diegoolliveira7 <diegoolliveira1999@gmail.com>
2024-05-29 23:01:54 -0300

    front

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,10 +5,24 @@ import { toast, ToastContainer} from "react-toastify"
 import GlobalStyle from './styles/global';
 import "react-toastify/dist/ReactToastfy.css";
 
+const Container = styled.div`
+  width: 100%;
+  max-width: 800px;
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column; 
+  align-items: center;
+  gap: 10px;
+`;
+
+const Title = styled.h2``;
 
 function App() {
   return (
     <>
+      <Container>
+          <Title>ATAS</Title>
+      </Container>
       <ToastContainer autoClose={3000} position={toast.POSITION.BOTTOM_LEFT} />
       <GlobalStyle />
     </>
